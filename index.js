239cffed1fd69dccd20d435443ffef6d46f84f63
nazar_zavrin <nzavrin1404@gmail.com>
2022-12-30 11:46:10 +0200

    v2

--- index.js
+++ index.js
@@ -1,56 +1,42 @@
 
-
+// Файлы connect-get-db.js и add-documents.js те же, что и в предыдущем примере
+// Файл connect-get-db.js тот же, что и в предыдущем примере
 // 📁 index.js
 const express = require('express');
 const app = express();
-// ↓ подключить модуль path
 const path = require('path');
 const fs = require('fs');
 
 const PORT = process.env.PORT ?? 3000;
 
-// ↓ обрабатываем get-запрос, получающий текущую страницу
-app.get('/', (req, res) => {
-    // ↓ отправить файл main.html из папки site, которая находится в текущей папке
-    res.send(fs.readFileSync(path.join(path.resolve(), "site", "main.html"), {"encoding": "utf8"}));
-})
+app.use(express.static(path.join(path.resolve(), 'site')));
 
-// ↓ обрабатываем get-запрос, читающий файл
-app.get('/read', (req, res) => {
-    // ↓ отправить файл manual.html из папки site, которая находится в текущей папке
-    fs.readFile(path.join(path.resolve(), "notes.txt"), {"encoding": "utf8"}, (error, content) => {
+app.get('/add-note', (req, res) => {
+    fs.appendFile(path.join(path.resolve(), "notes.txt"), "Запись\n", error => {
         if (error) {
-            res.send("Error: " + error);
-        } else {
-            res.send(`<div>${content}</div><a href='/'>На главную</a>`);// отослать строку-содержимое файла как ответ
+            throw error;
         }
+        res.send("Запись добавлена");
+    })
+})
+app.get('/get-notes', (req, res) => {
+    fs.readFile(path.join(path.resolve(), "notes.txt"), "utf-8", (error, data) => {
+        if (error) {
+            throw error;
+        }
+        res.send(data);
+    })
+})
+app.get('/clear', (req, res) => {
+    fs.writeFile(path.join(path.resolve(), "notes.txt"), "", error => {
+        if (error) {
+            throw error;
+        }
+        res.send("Записи стёрты");
     })
 })
 
-// ↓ обрабатываем get-запрос, добавляющий в файл
-app.get('/append', (req, res) => {
-    // ↓ отправить файл manual.html из папки site, которая находится в текущей папке
-    fs.appendFile(path.join(path.resolve(), "notes.txt"), "Запись ", (error) => {
-        if (error) {
-            res.send("Error: " + error);
-        } else {
-            res.send("<div>Запись добавлена</div><a href='/'>На главную</a>");
-        }
-    })
-})
-
-// ↓ обрабатываем get-запрос, стирающий содержимое файла
-app.get('/erase', (req, res) => {
-    // ↓ отправить файл manual.html из папки site, которая находится в текущей папке
-    fs.writeFile(path.join(path.resolve(), "notes.txt"), "", (error) => {
-        if (error) {
-            res.send("Error: " + error);
-        } else {
-            res.send("<div>Содержимое файла стёрто</div><a href='/'>На главную</a>");
-        }
-    })
-})
-
+// ↓ запустить сервер на порте PORT
 app.listen(PORT, () => {
     console.log(`Server has been started on port ${PORT}...`);
 })
@@ -64,13 +50,26 @@ app.listen(PORT, () => {
 
 
 
+// let name = ["David", "Adam", "Henry", "James", "John", "Lily", "Alice", "Daniel", "Kevin" ]
+// let surname = ["Hill", "Cliff", "King", "Cloud", 'Smith', ]
+/*
+finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+ */
+
+
+
+
 
 
 /* ↑ Выведет:
 
 */
+// <pre style="word-wrap: break-word; white-space: pre-wrap;"></pre>
 // let fruit = ['Персик', 'Банан', 'Яблоко', 'Апельсин', 'Груша', 'Ананас', 'Абрикос'];
-// let name = ["David", "Adam", "Henry", "James", "John", "Lily", "Alice", "Daniel", ]
+// let name = ["David", "Adam", "Henry", "James", "John", "Lily", "Alice", "Daniel", "Kevin" ]
 // let surname = ["Hill", "Cliff", "King", "Cloud", 'Smith', ]
 // ↑ ↓ 📁
 // let pathToChrome = 'C:/Program Files/Google/Chrome/Application/chrome.exe';
